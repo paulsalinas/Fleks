56348b714a92bca0d616475d09afd8d14b84d068
Paul <paul.salina@gmail.com>
2016-07-03 21:10:33 -0400

    add enter workout controller with ib binding with textfield

---
+++ Fleks/EnterWorkoutNameViewController.swift
@@ -0,0 +1,21 @@
+//
+//  EnterWorkoutNameViewController.swift
+//  Fleks
+//
+//  Created by Paul Salinas on 2016-07-03.
+//  Copyright © 2016 Paul Salinas. All rights reserved.
+//
+
+import UIKit
+
+class EnterWorkoutNameViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
