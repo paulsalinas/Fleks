4b81aebfdaaefbd71e9731348e030f8f7f4921dc
Paul <paul.salina@gmail.com>
2016-08-05 20:46:58 -0400

    add reactive cocoa wrappers to firebase

---
+++ Fleks/Firebase+ReactiveCocoa.swift
@@ -0,0 +1,38 @@
+//
+//  Firebase+ReactiveCocoa.swift
+//  Fleks
+//
+//  Created by Paul Salinas on 2016-08-05.
+//  Copyright © 2016 Paul Salinas. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import ReactiveCocoa
+import Result
+
+
+extension FIRDatabaseReference {
+    func rac_events(event: FIRDataEventType) -> SignalProducer<FIRDataSnapshot, NSError> {
+        return SignalProducer { observer, disposable in
+            let handle = self.observeEventType(event,
+                withBlock: { snapshot in
+                    observer.sendNext(snapshot)
+                },
+                withCancelBlock: { err in
+                    observer.sendFailed(err)
+                }
+            )
+            disposable += { self.removeObserverWithHandle(handle) }
+        }
+    }
+    
+    func rac_singleEvent(event: FIRDataEventType) -> SignalProducer<FIRDataSnapshot, NSError> {
+        return SignalProducer { observer, _ in
+            self.observeSingleEventOfType(event, withBlock: { snapshot in
+                observer.sendNext(snapshot)
+                observer.sendCompleted()
+            })
+        }
+    }
+}
