f19bd18ba2fbfc24d70d1e449c75c43ccd726c66
Paul <paul.salina@gmail.com>
2016-08-16 03:33:30 -0400

    removed commented out code

--- Fleks/ExerciseSetGroupTableViewController.swift
+++ Fleks/ExerciseSetGroupTableViewController.swift
@@ -186,23 +186,4 @@ class ExerciseSetGroupTableViewController: UITableViewController, ActivityOverla
     override func tableView(tableView: UITableView, editingStyleForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCellEditingStyle {
         return UITableViewCellEditingStyle.Delete
     }
-    
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
